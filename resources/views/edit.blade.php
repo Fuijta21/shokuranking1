6bf1b96f88878224aa398faa2ae39456c9ff8801
fujita39 <20t2134b@shinshu-u.ac.jp>
2022-09-13 16:09:14 +0900

    マイページ作成完了

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -18,9 +18,27 @@
         <form action="/mypage/" method='POST'>
             @csrf
             @method('PUT')
+            <div class='name'>
+                <h2>名前</h2>
+                <input type='text' name='user[name]' value='{{$old_user->name}}'>
+            </div>
+            <div class='sei'>
+                <label for="sei01" class="col-md-4 col-form label text-md-right">性別</label>
+                <div class='col-md-4'>
+                    <select class='form-control' id='sel01' name='user[gender]' value='{{$old_user->gender}}'>
+                        <option value="男性">男性</option>
+                        <option value="女性" selected>女性</option>
+                    </select>
+                </div>
+            </div>
+            <div class='age'>
+                <h2>年齢</h2>
+                <input type='text' name ='user[age]' value='{{$old_user->age}}'>
+                <p class='age_error' style="color:red">{{ $errors->first('user.age')}}</p>
+            </div>
             <div class='contet_title'>
                 <h2>紹介文</h2>
-                <input type='text' name ='user[shoukaibun]' value='{{$user->shoukaibun}}'>
+                <input type='text' name ='user[shoukaibun]' value='{{$old_user->shoukaibun}}'>
             </div>
             <input type='submit' value="保存">
         </form>
