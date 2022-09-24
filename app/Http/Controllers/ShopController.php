0f3331dd4f0aaf7cf85ed87d8c0ee956727a2a5d
fujita39 <20t2134b@shinshu-u.ac.jp>
2022-09-25 04:44:14 +0900

    yelp店名検索機能

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -1,155 +1,142 @@
 <?php
 
 namespace App\Http\Controllers;
+
+use Exception;
 use App\shop;
 use App\User;
 use Illuminate\Http\Request;
 
-
-
-
-
 class ShopController extends Controller
 {
-    
-    public function index(Request $request)
-    {
-        $keyword = $request->input('keyword');
-        
-        $query = shop::query();
-        
-        if(!empty($keyword)){
-            $query->where('keyword','LIKE',"%{$keyword}%")
-            ->orWhere('shop_name','LIKE',"%{$keyword}%");
-        }
-        
-        $shops = $query->get();
-        
-        
-        return view('index',compact('shops','keyword'));
-    }
-        public function profile(User $user)
-    {
-        return view(profile)->with(['user'=>$user]);
-    }
-    
+	public $API_HOST = 'https://api.yelp.com';
+	public $SEARCH_PATH = '/v3/businesses/search';
+	public $BUSINESS_PATH = '/v3/businesses/';  // Business ID will come after slash.
+	
+	public function index(Request $request)
+	{
+		$keyword = $request->input('keyword');
 
-    function request($host, $path, $url_params = array()) {
-    // Send Yelp API Call
-    try {
-        $curl = curl_init();
-        if (FALSE === $curl)
-            throw new Exception('Failed to initialize');
+		$query = shop::query();
 
-        $url = $host . $path . "?" . http_build_query($url_params);
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => $url,
-            CURLOPT_RETURNTRANSFER => true,  // Capture response.
-            CURLOPT_ENCODING => "",  // Accept gzip/deflate/whatever.
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 30,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "GET",
-            CURLOPT_HTTPHEADER => array(
-                "authorization: Bearer " . $GLOBALS['API_KEY'],
-                "cache-control: no-cache",
-            ),
-        ));
+		if (! empty($keyword)) {
+			$query->where('keyword', 'LIKE', "%{$keyword}%")
+	 ->orWhere('shop_name', 'LIKE', "%{$keyword}%");
+		}
 
-        $response = curl_exec($curl);
+		$shops = $query->get();
 
-        if (FALSE === $response)
-            throw new Exception(curl_error($curl), curl_errno($curl));
-        $http_status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-        if (200 != $http_status)
-            throw new Exception($response, $http_status);
+		return view('index', compact('shops', 'keyword'));
+	}
 
-        curl_close($curl);
-    } catch(Exception $e) {
-        trigger_error(sprintf(
-            'Curl failed with error #%d: %s',
-            $e->getCode(), $e->getMessage()),
-            E_USER_ERROR);
-    }
 
-    return $response;
-}
+	public function profile(User $user)
+	{
+		return view(profile)->with(['user' => $user]);
+	}
 
-/**
- * Query the Search API by a search term and location 
- * 
- * @param    $term        The search term passed to the API 
- * @param    $location    The search location passed to the API 
- * @return   The JSON response from the request 
- */
-function search($term, $location) {
-    $url_params = array();
-    
-    $url_params['term'] = $term;
-    $url_params['location'] = $location;
-    $url_params['limit'] = $GLOBALS['SEARCH_LIMIT'];
-    
-    return request($GLOBALS['API_HOST'], $GLOBALS['SEARCH_PATH'], $url_params);
-}
+	public function request($host, $path, $url_params = [])
+	{
+		// Send Yelp API Call
+		try {
+			$curl = curl_init();
+			if (false === $curl) {
+				throw new Exception('Failed to initialize');
+			}
 
-/**
- * Query the Business API by business_id
- * 
- * @param    $business_id    The ID of the business to query
- * @return   The JSON response from the request 
- */
-function get_business($business_id) {
-    $business_path = $GLOBALS['BUSINESS_PATH'] . urlencode($business_id);
-    
-    return request($GLOBALS['API_HOST'], $business_path);
-}
+			$url = $host.$path.'?'.http_build_query($url_params);
+			curl_setopt_array($curl, [
+				CURLOPT_URL => $url,
+				CURLOPT_RETURNTRANSFER => true,  // Capture response.
+				CURLOPT_ENCODING => '',  // Accept gzip/deflate/whatever.
+				CURLOPT_MAXREDIRS => 10,
+				CURLOPT_TIMEOUT => 30,
+				CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+				CURLOPT_CUSTOMREQUEST => 'GET',
+				CURLOPT_HTTPHEADER => [
+					'authorization: Bearer '.env('YELP_APP_KEY'),
+					'cache-control: no-cache',
+				],
+			]);
 
-/**
- * Queries the API by the input values from the user 
- * 
- * @param    $term        The search term to query
- * @param    $location    The location of the business to query
- */
-function query_api($term, $location) {     
-    $response = json_decode(search($term, $location));
-    $business_id = $response->businesses[0]->id;
-    
-    print sprintf(
-        "%d businesses found, querying business info for the top result \"%s\"\n\n",         
-        count($response->businesses),
-        $business_id
-    );
-    
-    $response = get_business($business_id);
-    
-    print sprintf("Result for business \"%s\" found:\n", $business_id);
-    $pretty_response = json_encode(json_decode($response), JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
-    print "$pretty_response\n";
-}
-function yelp_api($term,$location)
-{
-    assert($API_KEY, "Please supply your API key.");
+			$response = curl_exec($curl);
 
-// API constants, you shouldn't have to change these.
-$API_HOST = "https://api.yelp.com";
-$SEARCH_PATH = "/v3/businesses/search";
-$BUSINESS_PATH = "/v3/businesses/";  // Business ID will come after slash.
+			if (false === $response) {
+				throw new Exception(curl_error($curl), curl_errno($curl));
+			}
+			$http_status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+			if (200 != $http_status) {
+				throw new Exception($response, $http_status);
+			}
 
-// Defaults for our simple example.
-$DEFAULT_TERM = "dinner";
-$DEFAULT_LOCATION = "San Francisco, CA";
-$SEARCH_LIMIT = 3;
-    $longopts  = array(
-    "term::",
-    "location::",
-);
-    
-$options = getopt("", $longopts);
+			curl_close($curl);
+		} catch(Exception $e) {
+			trigger_error(sprintf(
+				'Curl failed with error #%d: %s',
+				$e->getCode(), $e->getMessage()),
+			E_USER_ERROR);
+		}
 
-$term = $options['term'] ?: $GLOBALS['DEFAULT_TERM'];
-$location = $options['location'] ?: $GLOBALS['DEFAULT_LOCATION'];
+		return $response;
+	}
 
-query_api($term, $location);
-}
-    
+	public function search($term, $location)
+	{
+		$url_params = [];
+
+		$url_params['term'] = $term;
+		$url_params['location'] = $location;
+		$url_params['limit'] = 10;
+
+		return $this->request($this->API_HOST, $this->SEARCH_PATH, $url_params);
+	}
+
+	public function get_business($business_id)
+	{
+		$business_path = $this->BUSINESS_PATH.urlencode($business_id);
+
+		return $this->request($this->API_HOST, $business_path);
+	}
+
+	public function query_api($term, $location)
+	{
+		$response = json_decode($this->search($term, $location));
+		$business_id = $response->businesses[0]->id;
+		dd($business_id);
+
+		$response = $this->get_business($business_id);
+
+		$pretty_response = json_encode(json_decode($response), JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+
+		return $pretty_response;
+	}
+
+	public function yelp_api(Request $request)
+	{	
+		$term = $request['term'];
+		$location=$request['location'];
+		assert($API_KEY, 'Please supply your API key.');
+
+		// API constants, you shouldn't have to change these.
+		$API_HOST = 'https://api.yelp.com';
+		$SEARCH_PATH = '/v3/businesses/search';
+		$BUSINESS_PATH = '/v3/businesses/';  // Business ID will come after slash.
+
+		// Defaults for our simple example.
+		$DEFAULT_TERM = 'dinner';
+		$DEFAULT_LOCATION = 'San Francisco, CA';
+		$SEARCH_LIMIT = 3;
+		$longopts = [
+			'term::',
+			'location::',
+		];
+
+		$pretty_response=$this->query_api($term, $location);
+		
+		// shop_information 1番目の検索結果
+		$shop_information=json_decode($pretty_response,false);
+		
+		
+		return view('search_result')->with(['shop_info'=>$shop_information]);
+	}
 }
