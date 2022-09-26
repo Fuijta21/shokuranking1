4ba835655293e20fb42145264bc0d5dacd95af61
fujita39 <20t2134b@shinshu-u.ac.jp>
2022-09-26 15:11:08 +0900

    googleマップ表示

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -25,6 +25,10 @@
                 <input type="submit" value="検索">
             </form>
         </div>
+
+        <div id='map' style='height:500px'></div>
+        <script src="{{ asset('/js/maps.js') }}"></script>
+        <script src="https:://maps.googleapis.com/maps/api/js?laguage=ja&region=JP&key=AIzaSyBCzdyBqWWIUDNxjhOj06D1QC_z6xMOoow&callback=initMap" async defer>
         
         <h1>店一覧</h1>
         <p class="edit">[<a href="/mypage">mypage</a>]</p>
